ebff060c0e4375434e415e3e7039d91337e2ebae
Luigi Leung <luigileung@gmail.com>
2018-04-16 23:35:24 -0700

    2.12 Handling multiple return values

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -3,7 +3,7 @@ pragma solidity ^ 0.14.9;
 import "./zombiefactory.sol";
 
 contract KittyInterface {
-    function getKitty(uint256 _id) external view returns (
+    function getKitty(uint256 _id) external view returns(
         bool isGestating,
         bool isReady,
         uint256 cooldownIndex,
@@ -22,7 +22,7 @@ contract ZombieFeeding is ZombieFactory {
     // CryptoKitties' contract address
     address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
     KittyInterface kittyContract = KittyInterface(ckAddress);
-    
+
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
@@ -32,4 +32,9 @@ contract ZombieFeeding is ZombieFactory {
         _createZombie("NoName", newDna);
     }
 
+    function feedOnKitty(uint _zombieId, uint _kittyId) public {
+        uint kittyDna;
+        (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna);
+    }
 }
