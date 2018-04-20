1382901b9f820661db5c15eab008f39e398f098d
Luigi Leung <luigileung@gmail.com>
2018-04-19 22:54:41 -0700

    5.6 ERC721: Transfer cont'd

--- zombieownership.sol
+++ zombieownership.sol
@@ -18,4 +18,8 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
         zombieToOwner[_tokenId] = _to;
         Transfer(_from, _to, _tokenId);
     }
+
+    function transfer(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
+        _transfer(msg.sender, _to, _tokenId);
+    }
 }
