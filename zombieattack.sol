0ac713eccc46673cb31d5da89f59e06f7284d87c
Luigi Leung <luigileung@gmail.com>
2018-04-19 17:11:31 -0700

    4.11 Zombie loss

--- zombieattack.sol
+++ zombieattack.sol
@@ -21,6 +21,10 @@ contract ZombieBattle is ZombieHelper {
             myZombie.level++;
             enemyZombie.lossCount++;
             feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+        } else {
+            myZombie.lossCount++;
+            enemyZombie.winCount++;
+            _triggerCooldown(myZombie);
         }
     }
 }
