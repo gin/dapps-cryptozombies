d07ff8fe2982daacfb85efe4200c0b6ae9f4fed9
Luigi Leung <luigileung@gmail.com>
2018-04-19 23:11:53 -0700

    5.8 ERC721: takeOwnership

--- zombieownership.sol
+++ zombieownership.sol
@@ -30,4 +30,10 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
         zombieApprovals[_tokenId] = _to;
         Approval(msg.sender, _to, _tokenId);
     }
+
+    function takeOwnership(uint256 _tokenId) public {
+        require(zombieApprovals[_tokenId] == msg.sender);
+        address owner = ownerOf(_tokenId);
+        _transfer(owner, msg.sender, _tokenId);
+    }
 }
