89f4e40314dbc7a13f3c2c51ddb76adff5e5dabd
Luigi Leung <luigileung@gmail.com>
2018-04-19 22:29:47 -0700

    5.3 balanceOf & ownerOf

--- zombieownership.sol
+++ zombieownership.sol
@@ -1,8 +1,14 @@
-pragma solidity ^0.4.19;
+pragma solidity ^ 0.4.19;
 
 import "./zombieattack.sol";
 import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
-    
+    function balanceOf(address _owner) public view returns(uint256 _balance) {
+        return ownerZombieCount[_owner];
+    }
+
+    function ownerOf(uint256 _tokenId) public view returns(address _owner) {
+        return zombieToOwner[_tokenId];
+    }
 }
